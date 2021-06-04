574f26b138eb290597025a0d5a52113596a5f42b
Louis Hamelers <louis.hamelers@alten.nl>
2021-06-04 06:53:51 +0200

    firebase environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB8eSvKjC93euHOBSLJ-deyV63FbuyjD08",
+    authDomain: "overcharge-a95da.firebaseapp.com",
+    projectId: "overcharge-a95da",
+    storageBucket: "overcharge-a95da.appspot.com",
+    messagingSenderId: "812239025759",
+    appId: "1:812239025759:web:fafb9dd8a908b4ed664dea"
+  }
 };
