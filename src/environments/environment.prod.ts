90382cb4802d5500022a3483fe792d049d8dbe43
Louis Hamelers <louis.hamelers@alten.nl>
2021-06-04 22:25:28 +0200

    add firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBuA5JGGLPNzmD0ll1NiKH14VRk0_9O-84",
+    authDomain: "suspicious-albatross.firebaseapp.com",
+    projectId: "suspicious-albatross",
+    storageBucket: "suspicious-albatross.appspot.com",
+    messagingSenderId: "154350053295",
+    appId: "1:154350053295:web:b263f639ba32f760a0f15a"
+  }
 };
