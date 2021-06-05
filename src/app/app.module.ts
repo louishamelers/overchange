96ebc1d8ba15141a507ddcdc976e0cc48f875bd9
Louis Hamelers <louis.hamelers@alten.nl>
2021-06-05 03:47:42 +0200

    add local firebase port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
+import { SETTINGS } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 
@@ -15,7 +16,15 @@ import { AppComponent } from './app.component';
     AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SETTINGS,
+      useValue: environment.production ? undefined : {
+        host: 'localhost:8080',
+        ssl: false
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
